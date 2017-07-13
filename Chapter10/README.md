f4d4e6b1359d6a6fe079a2a996dd94faad040078
packt-diwakar <diwakars@packtpub.com>
2017-07-13 17:11:18 +0530

    Code files added

---
+++ Chapter10/README.md
@@ -0,0 +1,34 @@
+## FlixOne Book store - a demo application showing the strength of microservices for .NET Core
+
+![PACKT PUB](https://dz13w8afd47il.cloudfront.net/sites/default/files/imagecache/ppv4_main_book_cover/B05017_MockupCover-01_0.png "Microservices for .NET Core")
+
+### How to setup project?
+Setting up this project is very simple, just need ```Visual Studio 2015 update3 or later```, ```.NET Core 1.1``` and here you're done.
+
+### How to build/run project?
+With the use of Visual Studio or .NET cli you can build and run the project
+
+### Fetaures
+This version of ```FlixOne book store``` is just a simple demostration of microservices, so, we tried to make it very simple with greta features:
+  - Item dashboard
+  - One click order
+  - Simple web interface developed in (ASP.NET Core 1.1)
+  
+### Upcoming features
+In the next versions we will add:
+  - Complete Login modules
+  - Complete User Management
+  - Separate Admin control
+  - Mobile interface (mobile app)
+  - Single page application
+
+### Contributors
+  - Gaurav Aroraa
+  - Kanwwar manish
+  - Lalit Kale
+
+### License
+MIT license
+
+### Further discussion/reading
+You can start discussion either by raising a new issue or from here: [microservices for .NET](http://microservicesfor.net)
---
+++ Chapter10/src/FlixOne.BookStore.ProductService/src/FlixOne.BookStore.ProductService/Startup.cs
@@ -0,0 +1,70 @@
+﻿using FlixOne.BookStore.ProductService.Contexts;
+using FlixOne.BookStore.ProductService.Persistence;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Swashbuckle.Swagger.Model;
+
+namespace FlixOne.BookStore.ProductService
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", true, true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddMvc();
+            //Inject
+            services.AddSingleton<IProductRepository, ProductRepository>();
+            services.AddDbContext<ProductContext>(
+                o => o.UseSqlServer(Configuration.GetConnectionString("ProductConnection")));
+
+            services.AddSwaggerGen();
+            services.ConfigureSwaggerGen(options =>
+            {
+                options.DescribeAllEnumsAsStrings();
+                options.SingleApiVersion(new Info
+                {
+                    Title = "Product HTTP API",
+                    // Version = "v1",
+                    Description = "The Product Service HTTP API",
+                    TermsOfService = "Terms Of Service"
+                });
+            });
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                        .AllowCredentials());
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            app.UseMvc();
+            app.UseSwaggerUi();
+        }
+    }
+}
